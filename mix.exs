735d3ee221e9355c023e54c20a4bce0c08720694
Ardhendu Das <ovidas98@gmail.com>
2025-04-16 15:43:10 +0600

    add uuid dependency

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule Employin.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:uniq, "~> 0.6.1"}
     ]
   end
 
