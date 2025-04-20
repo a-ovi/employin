afa3c04b3684143262d0ce5305d3839fdb78d8d0
Ardhendu Das <ovidas98@gmail.com>
2025-04-21 00:14:41 +0600

    add .iex.exs file with necessary aliases and imports for interactive elixir shell

---
+++ .iex.exs
@@ -0,0 +1,11 @@
+alias Employin.Users
+alias Employin.Users.User
+alias Employin.Events
+alias Employin.Events.Event
+alias Employin.LoginToken
+alias Employin.MailNotifier
+alias Employin.Repo
+
+import Ecto
+import Ecto.Query
+import Ecto.Changeset
