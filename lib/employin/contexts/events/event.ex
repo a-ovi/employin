d320c32eca5d0c65d81e3dcf23bbaf10f822bf89
Ardhendu Das <ovidas98@gmail.com>
2025-04-27 19:14:20 +0600

    feat: add validation for event date, hour, minute, and period fields

--- lib/employin/contexts/events/event.ex
+++ lib/employin/contexts/events/event.ex
@@ -7,6 +7,27 @@ defmodule Employin.Events.Event do
   @left "left"
   @valid_event_types [@left, @joined]
 
+  @time_points [:starting, :ending]
+  @components [:date, :hour, :minute, :period]
+
+  # Generate all fields
+  @date_fields (for point <- @time_points, component <- @components do
+                  :"#{point}_#{component}"
+                end)
+
+  # Pre-filter by component
+  @date_only_fields for point <- @time_points, do: :"#{point}_date"
+  @hour_only_fields for point <- @time_points, do: :"#{point}_hour"
+  @minute_only_fields for point <- @time_points, do: :"#{point}_minute"
+  @period_only_fields for point <- @time_points, do: :"#{point}_period"
+
+  # Define field types for the date-related fields. all are strings here
+  @date_field_types Map.from_keys(@date_fields, :string)
+
+  @hours Enum.map(1..12, &Integer.to_string/1)
+  @minutes Enum.map(0..59, &Integer.to_string/1)
+  @periods ["am", "pm"]
+
   schema "events" do
     field :type, :string
     field :time, :utc_datetime
@@ -22,6 +43,16 @@ defmodule Employin.Events.Event do
     |> validate_inclusion(:type, @valid_event_types)
   end
 
+  def form_changeset(params) do
+    {%{}, @date_field_types}
+    |> cast(params, @date_fields)
+    |> validate_required(@date_fields)
+    |> validate_date(@date_only_fields)
+    |> validate_hour(@hour_only_fields)
+    |> validate_minute(@minute_only_fields)
+    |> validate_period(@period_only_fields)
+  end
+
   def joined() do
     @joined
   end
@@ -30,4 +61,53 @@ defmodule Employin.Events.Event do
     @left
   end
 
+  def validate_date(changeset, fields) do
+    fields = List.wrap(fields)
+
+    Enum.reduce(fields, changeset, fn field, acc ->
+      validate_change(acc, field, &check_iso_date_format/2)
+    end)
+  end
+
+  def validate_hour(changeset, fields) do
+    validate_inclusion_for_multiple_fields(
+      changeset,
+      fields,
+      @hours,
+      "Hour must be between 1 to 12"
+    )
+  end
+
+  def validate_minute(changeset, fields) do
+    validate_inclusion_for_multiple_fields(
+      changeset,
+      fields,
+      @minutes,
+      "Minute must be between 0 to 59"
+    )
+  end
+
+  def validate_period(changeset, fields) do
+    validate_inclusion_for_multiple_fields(
+      changeset,
+      fields,
+      @periods,
+      "Period must be AM or PM"
+    )
+  end
+
+  defp validate_inclusion_for_multiple_fields(changeset, fields, valid_values, message) do
+    fields = List.wrap(fields)
+
+    Enum.reduce(fields, changeset, fn field, acc ->
+      validate_inclusion(acc, field, valid_values, message: message)
+    end)
+  end
+
+  defp check_iso_date_format(field, value) do
+    case Date.from_iso8601(value) do
+      {:ok, _} -> []
+      {:error, _} -> [{field, "Date is invalid!"}]
+    end
+  end
 end
