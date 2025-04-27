b6aa27e220ad31052da98030e56694fe37157abe
Ardhendu Das <ovidas98@gmail.com>
2025-04-27 19:14:44 +0600

    feat: enhance event creation form with validation and improved date handling

--- lib/employin_web/live/home/home_live.ex
+++ lib/employin_web/live/home/home_live.ex
@@ -15,10 +15,6 @@ defmodule EmployinWeb.HomeLive do
       Phoenix.PubSub.subscribe(Employin.PubSub, "events")
     end
 
-    # event_form =
-    #   %{"start_date" => Date.utc_today(), "end_date" => Date.utc_today()}
-    #   |> to_form()
-
     socket =
       socket
       |> assign(:page_title, "Home")
@@ -27,7 +23,6 @@ defmodule EmployinWeb.HomeLive do
       |> assign(:events, events)
       |> assign(:current_status, current_status)
       |> assign(:show_event_modal, false)
-      # |> assign(:event_form, %{})
 
     {:ok, socket}
   end
@@ -41,6 +36,7 @@ defmodule EmployinWeb.HomeLive do
     end
 
     events = Events.get_events()
+
     socket =
       socket
       |> assign(:current_status, Event.joined())
@@ -58,6 +54,7 @@ defmodule EmployinWeb.HomeLive do
     end
 
     events = Events.get_events()
+
     socket =
       socket
       |> assign(:current_status, Event.left())
@@ -68,28 +65,100 @@ defmodule EmployinWeb.HomeLive do
 
   @impl true
   def handle_event("show_event_modal", _params, socket) do
-    event_form =
-      %{"start_date" => Date.utc_today(), "end_date" => Date.utc_today()}
-      |> to_form()
+    today = Date.utc_today()
+
+    event_form = %{
+      "starting_date" => today,
+      "ending_date" => today
+    }
+
+    event_form = to_form(event_form, as: "date_form")
 
     socket =
       socket
       |> assign(:show_event_modal, true)
-      |> assign(:event_form, event_form)
+      |> assign(:form, event_form)
 
     {:noreply, socket}
   end
 
   @impl true
-  def handle_event("save_event", params, socket) do
-    IO.inspect(params, label: "--------->")
+  def handle_event("validate-form", %{"date_form" => form_fields} = params, socket) do
+    IO.inspect(params)
+
+    form =
+      form_fields
+      |> Event.form_changeset()
+      |> to_form(action: :validate, as: :date_form)
 
     socket =
       socket
-      |> assign(:show_event_modal, false)
+      |> assign(form: form)
 
+    IO.inspect(form, label: "validate------------------------> ")
 
     {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("submit-form", %{"date_form" => form_fields} = _params, socket) do
+    form_changeset = Event.form_changeset(form_fields)
+
+    case form_changeset.valid? do
+      false ->
+        form = to_form(form_changeset, action: :validate, as: :date_form)
+        socket = assign(socket, form: form)
+        {:noreply, socket}
+
+      true ->
+        %{starting: starting_dt, ending: ending_dt} =
+          create_utc_date_time_from_form_fields(form_fields, socket.assigns.tz_offset)
+
+        if not DateTime.after?(ending_dt, starting_dt) do
+          form_changeset =
+            form_changeset
+            |> Ecto.Changeset.add_error(
+              :ending_date_time,
+              "ending date-time must be after starting date-time"
+            )
+            |> Map.put(:action, :validate)
+
+          form = to_form(form_changeset, as: :date_form)
+          socket = assign(socket, form: form)
+          {:noreply, socket}
+        else
+          if Events.can_insert?(socket.assigns.user_id, starting_dt, ending_dt) do
+            user_id = socket.assigns.user_id
+            join_attrs = %{type: Event.joined(), time: starting_dt}
+            leave_attrs = %{type: Event.left(), time: ending_dt}
+
+            for attrs <- [join_attrs, leave_attrs] do
+              with {:ok, event} <- Events.create_event(user_id, attrs) do
+                Phoenix.PubSub.broadcast(Employin.PubSub, "events", {:new_event, event})
+              end
+            end
+
+            socket =
+              socket
+              |> assign(:show_event_modal, false)
+              |> put_flash(:info, "Event Created Successfully!")
+            {:noreply, socket}
+          else
+            form_changeset =
+              form_changeset
+              |> Ecto.Changeset.add_error(
+                :overlap,
+                "overlaps with existing events"
+              )
+              |> Map.put(:action, :validate)
+
+            form = to_form(form_changeset, as: :date_form)
+            IO.inspect(form, label: "form->>>>>>>>>>>>>>>> ")
+            socket = assign(socket, form: form)
+            {:noreply, socket}
+          end
+        end
+    end
   end
 
   @impl true
@@ -97,6 +166,7 @@ defmodule EmployinWeb.HomeLive do
     socket =
       socket
       |> assign(:show_event_modal, false)
+
     {:noreply, socket}
   end
 
@@ -106,7 +176,39 @@ defmodule EmployinWeb.HomeLive do
     {:noreply, assign(socket, :events, events)}
   end
 
-  def format_time(date_time, tz_offset \\ 0) do
+  def create_utc_date_time_from_form_fields(params, tz_offset) do
+    starting = process_datetime(params, "starting", tz_offset)
+    ending = process_datetime(params, "ending", tz_offset)
+
+    %{starting: starting, ending: ending}
+  end
+
+  defp process_datetime(params, prefix, tz_offset) do
+    date = params["#{prefix}_date"]
+    hour = String.to_integer(params["#{prefix}_hour"] || "0")
+    minute = String.to_integer(params["#{prefix}_minute"] || "0")
+    period = params["#{prefix}_period"]
+
+    hour =
+      case {hour, period} do
+        {12, "am"} -> 0
+        {12, "pm"} -> 12
+        {h, "pm"} -> h + 12
+        {h, _} -> h
+      end
+
+    {:ok, date} = Date.from_iso8601(date)
+    {:ok, time} = Time.new(hour, minute, 0)
+    {:ok, dt} = DateTime.new(date, time)
+
+    result = DateTime.shift(dt, minute: -tz_offset)
+
+    if Mix.env() == :dev, do: IO.inspect(result, label: "#{prefix} date ->>>>>>")
+
+    result
+  end
+
+  defp format_time(date_time, tz_offset) do
     date_time
     |> DateTime.shift(minute: tz_offset)
     |> Calendar.strftime("%d %b %I:%M %p")
@@ -115,5 +217,4 @@ defmodule EmployinWeb.HomeLive do
   defp get_tz_offset(socket) do
     get_connect_params(socket)["tz_offset"] || 0
   end
-
 end
